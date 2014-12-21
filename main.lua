4c153342fcc09b23d7d2d159d1c5cbf990c8761e
Sam Kniffer <rtmonster@null.net>
2014-12-21 13:10:42 -0700

    new code stuff

---
+++ main.lua
@@ -0,0 +1,27 @@
+function love.load()
+	love.graphics.setBackgroundColor( 127, 127, 127 )
+end
+
+function love.draw()
+end
+
+function love.update(dt)
+end
+
+function love.focus(bool)
+end
+
+function love.keypressed( key, unicode )
+end
+
+function love.keyreleased( key, unicode )
+end
+
+function love.mousepressed( x, y, button )
+end
+
+function love.mousereleased( x, y, button )
+end
+
+function love.quit()
+end
